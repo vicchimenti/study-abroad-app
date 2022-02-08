8845d04c3a7f400e52a9bd53dbd4424d05c99525
vicchimenti <chimenti@seattleu.edu>
2022-02-08 06:58:28 -0800

    create generic media tag

--- v9-text-html-organizer.js
+++ v9-text-html-organizer.js
@@ -1,5 +1,9 @@
 // PSL
-var version="21.0";eval(function(version){var minImports=JavaImporter(com.terminalfour.media.IMediaManager,com.terminalfour.spring.ApplicationContextProvider,com.terminalfour.version.Version);with(minImports){var mm=ApplicationContextProvider.getBean(IMediaManager),media=mm.get(3101315,language,new Version(version)).getMedia(),s=new java.util.Scanner(media).useDelimiter("\\A");return String(s.hasNext()?s.next():"")}}(version))
+var version = "21.0";
+eval(function(version) { var minImports = JavaImporter(com.terminalfour.media.IMediaManager, com.terminalfour.spring.ApplicationContextProvider, com.terminalfour.version.Version);
+    with(minImports) { var mm = ApplicationContextProvider.getBean(IMediaManager),
+            media = mm.get(3101315, language, new Version(version)).getMedia(),
+            s = new java.util.Scanner(media).useDelimiter("\\A"); return String(s.hasNext() ? s.next() : "") } }(version))
 importClass(com.terminalfour.navigation.IServerSideLinkManager)
 importClass(com.terminalfour.hierarchy.IHierarchyManager)
 importClass(com.terminalfour.content.IContentManager)
@@ -14,21 +18,21 @@ try {
     var ssLink = ssLinkManager.get(ssid, section.getID(), content.getID(), language)
     var section = hierarchyManager.get(ssLink.getToSectionID())
     var sContentList = section.getContentSections()
-    
+
     document.write('<div class="studyAbroadOrganizerLocation d-flex flex-column flex-md-row flex-wrap justify-content-center card-group">')
     var cssClass = 'locationFeedItem profileItem card shadow col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-4'
-    for(var i = 0; i < sContentList.size(); i++) {
+    for (var i = 0; i < sContentList.size(); i++) {
         try {
             // Get content entry
             var targetContent = contentManager.get(section.getContentSections().get(i).getContentID(), language)
-            var imageStr = function () {
+            var imageStr = function() {
                 if (targetContent.get('Web Image ID number') == '') {
                     return targetContent.get('Web Image URL')
                 } else {
                     return Base64.encode(getMedia(targetContent.get('Web Image ID number')).readAllBytes())
                 }
             }()
-            var imageDescription = function () {
+            var imageDescription = function() {
                 if (targetContent.get('Alt Text') == '' && !(targetContent.get('Web Image ID Number') == '')) {
                     return mediaManager.get(targetContent.get('Web Image ID number'), language).getDescription()
                 } else {
@@ -36,14 +40,14 @@ try {
                 }
             }()
 
-            document.write('<div id="source" class="' + cssClass + '" title="' + targetContent.get('Name') + '" id="id' + targetContent.getID() + 
-            '" data-position-default="ZoneA" data-position-selected="ZoneA">' + 
-            '<span class="cardImageWrapper"><img src="data:image/jpeg;base64,' + imageStr + '" class="card-img-top"' +
-            ' alt="' + imageDescription + '"></span> <div class="card-body"><h3 class="card-title">' + 
-            '<a href="' + targetContent.get('Program Website') + '">' + targetContent.get('Program Title') + '</a></h3>' +
-            '<p class="card-text margin0 subtext">' + targetContent.get('City') + ', ' + targetContent.get('Country') + 
-            '</p> <p class="card-text margin0">' + targetContent.get('Program Type') + '</p> <p class="card-text">' + targetContent.get('Program Fee') + '</p>' + 
-            '</div><div class="card-footer"><span class="locationRegion">' + targetContent.get('Region') + '</span></div></div>')
+            document.write('<div id="source" class="' + cssClass + '" title="' + targetContent.get('Name') + '" id="id' + targetContent.getID() +
+                '" data-position-default="ZoneA" data-position-selected="ZoneA">' +
+                '<span class="cardImageWrapper"><img src="data:image/jpeg;base64,' + imageStr + '" class="card-img-top"' +
+                ' alt="' + imageDescription + '"></span> <div class="card-body"><h3 class="card-title">' +
+                '<a href="' + targetContent.get('Program Website') + '">' + targetContent.get('Program Title') + '</a></h3>' +
+                '<p class="card-text margin0 subtext">' + targetContent.get('City') + ', ' + targetContent.get('Country') +
+                '</p> <p class="card-text margin0">' + targetContent.get('Program Type') + '</p> <p class="card-text">' + targetContent.get('Program Fee') + '</p>' +
+                '</div><div class="card-footer"><span class="locationRegion">' + targetContent.get('Region') + '</span></div></div>')
         } catch (e) {
             document.write(e)
         }
